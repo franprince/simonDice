21520366c4de35543108e923dc135668094abef9
Francisco Prince <fran.p.21@gmail.com>
2020-01-07 10:49:49 -0300

    agrega sweetalert

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const   $cuadro         = document.querySelectorAll(".cuadrado"),
-        $botonEmpezar   = document.querySelector("#boton-empezar"),
-        $mostrarPlayer  = document.querySelector(".anuncios h2"),
-        $mostrarRonda   = document.querySelector(".anuncios h3");
+const $cuadro = document.querySelectorAll(".cuadrado"),
+    $botonEmpezar = document.querySelector("#boton-empezar"),
+    $mostrarPlayer = document.querySelector(".anuncios h2"),
+    $mostrarRonda = document.querySelector(".anuncios h3");
 
 const colores = [
     "amarillo",
@@ -10,54 +10,65 @@ const colores = [
     "rojo"
 ];
 
-let secuenciaComputadora    = [],
+let secuenciaComputadora = [],
 
-    secuenciaUsuario        = [],
+    secuenciaUsuario = [],
 
-    ronda                   = 1;
+    ronda = 0;
 
 function obtenerCuadroAleatorio() {
     const indice = Math.floor(Math.random() * colores.length);
     return colores[indice];
 };
 
-function anunciarJugador(jugador){
+function anunciarJugador(jugador) {
     $mostrarPlayer.textContent = `Juega ${jugador}`;
-}
-function anunciarRonda(ronda){
-    $mostrarRonda.textContent = `Ronda: ${ronda}`;
-}
-
-function resaltar(color){
-    document.querySelector("#"+color).classList.add("destacar");
-    setTimeout(()=>document.querySelector("#"+color).classList.remove("destacar"), 1000);
 };
 
-function desbloquearUsuario(){
-    $cuadro.forEach((element)=>{
-        element.addEventListener("click", function(){
-            resaltar(element.id);
-            secuenciaUsuario.push(element.id);
-            checkUserInput();
-        });
+function anunciarRonda(ronda) {
+    $mostrarRonda.textContent = `Ronda: ${ronda}`;
+};
+
+function resaltar(color) {
+    document.querySelector("#" + color).classList.add("destacar");
+    setTimeout(() => document.querySelector("#" + color).classList.remove("destacar"), 300);
+};
+
+function desbloquearUsuario() {
+    $cuadro.forEach((e) => {
+        e.onclick = function () {
+            const $color = e.id;
+            manejarInputUsuario($color);
+        };
     });
 };
 
-function checkUserInput(){
-    if(secuenciaComputadora[ronda] === secuenciaUsuario[ronda]){
-        manejarRonda()
-    }else{
-        console.log("tequivocastes");
-        return false;
+function manejarInputUsuario(color) {
+    resaltar(color);
+    console.log(color)
+    secuenciaUsuario.push(color);
+    checkUserInput();
+
+    if (secuenciaUsuario.length === secuenciaComputadora.length) {
+        bloquearInputUsuario();
+        setTimeout(manejarRonda, 1000);
+    };
+};
+
+function checkUserInput() {
+    const i = ronda - 1;
+    if (secuenciaComputadora[i] !== secuenciaUsuario[i]) {
+        gameOver();
     };
 };
 
 function bloquearInputUsuario() {
-   $cuadro.onclick = function(){};
+    $cuadro.forEach((cuadrito) => {
+        cuadrito.onclick = function () { };
+    });
 };
 
-function manejarRonda(){
-
+function manejarRonda() {
     anunciarRonda(ronda);
 
     anunciarJugador("computadora");
@@ -69,27 +80,43 @@ function manejarRonda(){
 
     const RETRASO_TURNO_JUGADOR = (secuenciaComputadora.length + 1) * 1000;
 
-    secuenciaComputadora.forEach(function($cuadro, index) {
+    secuenciaComputadora.forEach(function ($cuadro, index) {
         const RETRASO_MS = (index + 1) * 1000;
-        setTimeout(function() {
-          resaltar($cuadro);
+        setTimeout(function () {
+            resaltar($cuadro);
         }, RETRASO_MS);
-      });
+    });
 
-      setTimeout(function(){
+    setTimeout(function () {
         anunciarJugador("usuario");
         desbloquearUsuario();
-      }, RETRASO_TURNO_JUGADOR);
+    }, RETRASO_TURNO_JUGADOR);
 
-      secuenciaUsuario = [];
-      ronda++;
-      anunciarRonda(ronda);
+    secuenciaUsuario = [];
+    ronda++;
+    anunciarRonda(ronda);
 };
 
-function iniciarJuego(){
-    secuenciaComputadora    = [];
-    secuenciaUsuario        = [];
+function reset() {
+    secuenciaUsuario = [];
+    secuenciaComputadora = [];
+    ronda = 0;
+};
+
+function iniciarJuego() {
+    reset();
     manejarRonda();
 };
 
-$botonEmpezar.onclick = iniciarJuego();
+function gameOver(){
+
+    Swal.fire({
+        icon: 'error',
+        title: 'Fin del juego',
+        text:  `Perdiste en la ronda ${ronda}.`,
+        footer: '<a href>Volver a jugar</a>'
+      });
+      Swal.fire.footer.onclick = reset;
+}
+
+$botonEmpezar.onclick = iniciarJuego;
